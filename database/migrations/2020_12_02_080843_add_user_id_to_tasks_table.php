155a32fa32dc0f87fd441a42bf98b5193d544953
amirhosseinadli-laptop <amirhossein.adli@gmail.com>
2020-12-02 15:24:36 +0330

    models and migrations

---
+++ database/migrations/2020_12_02_080843_add_user_id_to_tasks_table.php
@@ -0,0 +1,21 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddUserIdToTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('tasks', function (Blueprint $table) {
+            $table->foreignIdFor(User::class)->after('id')->references('id')->on('users')->cascadeOnDelete();
+        });
+    }
+}
